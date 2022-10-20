b1cc522ebaa87b60c5d5508b5d2eb3c379807a5f
chris25 <cristo_alvarez250@hotmail.com>
2022-10-20 04:05:16 -0600

    Functional update ROOM, insert function fixed up

--- app/src/main/java/com/example/locationmaker2/MainActivity.kt
+++ app/src/main/java/com/example/locationmaker2/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.content.Intent
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.*
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.locationmaker2.adapter.LocationAdapter
+import com.example.locationmaker2.data.database.models.LocationsListData
 import com.example.locationmaker2.databinding.ActivityMainBinding
 import com.example.locationmaker2.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,15 +23,22 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint //Check what is Hilt
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+   /* private var dataMutableList: MutableList<LocationData> =
+        dataProviderLocation.dataLocationList.toMutableList()
+    private lateinit var adapterFilter:LocationAdapter*/
     private val viewModel : MainViewModel by viewModels() //Check why is working
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //ENLACE MODELO CON LIVEDATA
-        /*viewModel = ViewModelProvider(this).get()
-        viewModel.Iniciar()*/
+        /*binding.edtSearch.addTextChangedListener { userFilterText ->
+         val dataFilterItems =   dataMutableList.filter { dataDirection ->
+             dataDirection.streetDirection.contains(userFilterText.toString(), ignoreCase = true) }
+            adapterFilter.updaeDataFilter(dataFilterItems)
+        }*/
+
+
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -57,5 +69,6 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("location", locationData)
         startActivity(intent)
     }
+
 
 }
--- app/src/main/java/com/example/locationmaker2/adapter/LocationAdapter.kt
+++ app/src/main/java/com/example/locationmaker2/adapter/LocationAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.locationmaker2.adapter
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.locationmaker2.LocationData
 import com.example.locationmaker2.R
@@ -11,7 +13,7 @@ import com.example.locationmaker2.data.database.models.LocationsListData
 
 
 class LocationAdapter(
-    private val dataLocationList:List<LocationData>,
+    private var dataLocationList:List<LocationData>,
     private val onClickListener:(LocationData) -> Unit
 ) : RecyclerView.Adapter<LocationViewHolder>() {
 
@@ -27,5 +29,11 @@ class LocationAdapter(
     }
 
     override fun getItemCount(): Int = dataLocationList.size
+
+
+    /*fun updaeDataFilter(dataLocationList:List<LocationData>){
+        this.dataLocationList = dataLocationList
+        notifyDataSetChanged()
+    }*/
 
 }
